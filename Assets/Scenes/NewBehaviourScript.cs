9e2bec3b0cf56ec3954e498dc583314a763de5c6
Thomas Allen <tomtinderbundle@gmail.com>
2021-03-24 09:46:48 -0700

    Moved aorund the project, made a test scene, made progress on random generation script

--- Assets/Scenes/NewBehaviourScript.cs
+++ Assets/Scenes/NewBehaviourScript.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AqwesomeScrpt : MonoBehaviour
+public class NewBehaviourScript : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -13,6 +13,6 @@ public class AqwesomeScrpt : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        print("I am awesome");
+        
     }
 }
---
+++ Assets/Scripts/PlatformSpawner.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformSpawner : MonoBehaviour
+{
+    [SerializeField] GameObject basicPlatform;
+    [SerializeField] GameObject parentObj;
+    [SerializeField] Transform playerTransform;
+    
+
+    private Vector2 randPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 20; i++)
+            SpawnPlatform(playerTransform);
+    }
+
+    public void SpawnPlatform(Transform playerTransform)
+    {
+        GameObject[] previousPlatforms = GetComponentsInChildren<GameObject>();
+        randPos = GenerateRandom();
+        GameObject temp = Instantiate(basicPlatform, randPos, Quaternion.identity);
+        temp.transform.parent = parentObj.transform;
+
+        for (int i = 0; i < previousPlatforms.Length; i++)
+        {
+            if (temp.GetComponent<BoxCollider2D>().bounds.Intersects(previousPlatforms[i].GetComponent<BoxCollider2D>().bounds))
+            {
+                Destroy(temp);
+                SpawnPlatform(playerTransform);
+            }
+        }
+    }  
+
+    private static Vector2 GenerateRandom()
+    {
+        Vector2 randPos;
+        randPos.x = Random.Range(-14.5f, 14.5f);
+        randPos.y = Random.Range(-2.5f, 2.5f);
+        return randPos;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        
+    }
+}
