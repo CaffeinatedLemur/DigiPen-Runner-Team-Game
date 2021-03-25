22a70cea11ddf7d375db39b36df858c36b2fbdb2
Thomas Allen <tomtinderbundle@gmail.com>
2021-03-25 16:55:43 -0700

    First Version of Tile Generation

--- Assets/Scripts/PlatformSpawner.cs
+++ Assets/Scripts/PlatformSpawner.cs
@@ -7,31 +7,46 @@ public class PlatformSpawner : MonoBehaviour
     [SerializeField] GameObject basicPlatform;
     [SerializeField] GameObject parentObj;
     [SerializeField] Transform playerTransform;
-    
 
+
+    private Vector3[,] platformPositions = new Vector3[25, 10];
     private Vector2 randPos;
+
     // Start is called before the first frame update
     void Start()
     {
+        for (int i = 0; i < 25; i++)
+        {
+            for (int p = 0; p < 10; p++)
+            {
+                platformPositions[i, p] = new Vector3(i, p, -10);
+            }
+        }
+
         for (int i = 0; i < 20; i++)
             SpawnPlatform(playerTransform);
     }
 
     public void SpawnPlatform(Transform playerTransform)
     {
-        GameObject[] previousPlatforms = GetComponentsInChildren<GameObject>();
+        GameObject temp = Instantiate(basicPlatform, randPos, Quaternion.identity);
+        temp.transform.parent = parentObj.transform;
+        temp.transform.localPosition = platformPositions[Random.Range(0, 25), Random.Range(0, 10)];
+        /*
+        BoxCollider2D[] previousPlatforms = GetComponentsInChildren<BoxCollider2D>();
         randPos = GenerateRandom();
         GameObject temp = Instantiate(basicPlatform, randPos, Quaternion.identity);
         temp.transform.parent = parentObj.transform;
 
         for (int i = 0; i < previousPlatforms.Length; i++)
         {
-            if (temp.GetComponent<BoxCollider2D>().bounds.Intersects(previousPlatforms[i].GetComponent<BoxCollider2D>().bounds))
+            if (temp.GetComponent<BoxCollider2D>().bounds.Intersects(previousPlatforms[i].bounds))
             {
-                Destroy(temp);
+                temp.SetActive(false);
                 SpawnPlatform(playerTransform);
             }
         }
+        */
     }  
 
     private static Vector2 GenerateRandom()
